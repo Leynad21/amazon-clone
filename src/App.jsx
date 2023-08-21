806cd985a481970785af280af4dd3f90f73d0af6
Dany <dany21sayan_@hotmail.com>
2023-08-21 12:45:53 +0200

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import NavBar from "./components/NavBar"
+import SearchResults from "./components/SearchResults"
+import ProductPage from "./components/ProductPage"
+import Checkout from "./components/Checkout"
 
 function App() {
 
   return (
     <>
       <Router>
+        <NavBar />
         <Routes>
           <Route exact path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchResults />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </Router>
     </>
---
+++ src/components/ProductPage.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { callAPI } from '../utils/CallApi'
+import ProductDetails from './ProductDetails'
+
+const ProductPage = () => {
+
+    const { id } = useParams()
+
+    const [product, setProduct] = useState(null)
+
+    const getProduct = () => {
+        callAPI(`data/products.json`)
+            .then((productResults) => {
+                setProduct(productResults[id])
+            })
+    }
+
+    useEffect(() => {
+        getProduct()
+    }, [])
+
+    if (!product?.title) return <h1>Loading Product</h1>
+
+    return (
+        product &&
+        <div className='h-screen bg-amazonclone-background'>
+            <div className='min-w-[1000px] max-w-[1500px] m-auto bg-orange-400'>
+                <div className='grid grid-cols-10 gap-2'>
+                    <div className=' col-span-3 p-8 rounded bg-white m-auto'>
+                        <img src={`${product.image}`} />
+                    </div>
+                    <div className='col-span-5 p-4 rounded bg-white divide-y divide-gray-400'>
+                        <div className='mb-3'>
+                            <ProductDetails
+                                product={product}
+                                ratings={false}
+                            />
+                        </div>
+                        <div></div>
+                    </div>
+                    <div className='col-span-2'>Right</div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProductPage
